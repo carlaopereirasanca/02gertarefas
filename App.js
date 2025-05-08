6b654266985864c387b82b60b61195251776e8f5
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2025-05-08 19:01:39 -0300

    comecando apagar

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+
+import ItemTarefa from './components/ItemTarefa';
+import DigitaTarefa from './components/DigitaTarefa';
+
+import Toast from 'react-native-toast-message';
 
 export default function App() {
+
+  // Estado para armazenar a lista de tarefas:
+  const [listaTarefas, setListaTarefas] = useState([]);
+
+  // Função que usa o setter do estado listaTarefas para inserir
+  // o texto da tarefa, recebido por parâmetro, na lista.
+  // Esta função será passada para o componente DigitaTarefa via
+  // props 'onAdicionaTarefa' para que o texto digitado lá neste
+  // componente seja adicionado na lista de tarefas que está aqui
+  // em App.js:
+
+  function adicionaTarefa(textoTarefa) {
+    setListaTarefas( listaAtual => [ 
+      ...listaAtual, 
+      { text: textoTarefa, id: Math.random().toString() } 
+    ] );
+  }
+
+  function apagaTarefa() {
+    // Por enquanto, vamos apenas exibir no console:
+    console.log("Apagando...");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.appContainer}>
+
+      <DigitaTarefa onAdicionaTarefa={adicionaTarefa} />
+
+      <View style={styles.goalsContainer}>
+
+        <FlatList
+
+          data = {listaTarefas}
+          keyExtractor={ (item, index) => { return item.id; } }
+          renderItem = { (itemData) => {
+            return <ItemTarefa 
+              text={itemData.item.text} 
+              aoApagar={apagaTarefa}
+            />;
+          } }
+
+        />
+
+      </View>
+
+      <Toast />
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const styles = StyleSheet.create(
+  {
+    appContainer: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16
+    },
+    goalsContainer: {
+        flex: 5
+    }
   },
-});
+);
---
+++ components/ItemTarefa.js
@@ -0,0 +1,34 @@
+
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function ItemTarefa(props) {
+
+    return (
+
+        <Pressable onPress={props.aoApagar}>
+            <View style={styles.goalItem}>
+                <Text style={styles.goalText}>
+                    {props.text}
+                </Text>
+            </View>
+        </Pressable>
+    );
+};
+
+export default ItemTarefa;
+
+const styles = StyleSheet.create(
+    {
+        goalItem: {
+            margin: 8,
+            padding: 8,
+            borderRadius: 6,
+            backgroundColor: '#487d76'
+        },
+        goalText: {
+            fontSize: 20,
+            color: 'white'
+        }
+    },
+);
+
